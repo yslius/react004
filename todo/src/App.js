8de786f34236d07b2aafeb1ca7f136fafab6d44b
nakagawa <yslius81[gmail.com>
2021-12-09 16:57:32 +0900

    first commit

---
+++ todo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Todo from './components/Todo';
+import './styles/main.css';
+
+function App() {
+  return (
+    <div className="container is-fluid">
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
